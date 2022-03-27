1625b578a7ad6d125f66a4929d80c6d34b105529
mouakong12 <101863295+mouakong12@users.noreply.github.com>
2022-03-27 18:45:54 +0700

    Add files via upload

---
+++ Caculator.java
@@ -0,0 +1,14 @@
+ class Caculator{
+    public static int plus(int a,int b) {
+       return (a+b);
+    }
+    public static int minus(int c,int d) {
+        return (c-d);
+    }
+    public static double multiply(double e,double f) {
+        return (e*f);
+    }
+    public static double division(double g,double h){
+        return (g/h);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+      System.out.println(Caculator.plus(4,6));
+      System.out.println(Caculator.minus(6,3));
+      System.out.println(Caculator.multiply(4,6));
+      System.out.println(Caculator.division(4,2));
+    }
+}
